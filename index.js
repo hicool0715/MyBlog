3633775061f98127d3e637574a3424d1758a9a00
hicool0715 <18870405316@163.com>
2025-02-14 09:52:57 +0800

    feat: footer动态宽度

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ function adaptPageWidth() {
   function updateWidth() {
     let pageWidth = this.window.innerWidth;
     const body_page = this.document.getElementById("body-page");
+    const ele_footer = this.document.getElementsByTagName("footer")[0];
     if (pageWidth > 1200) {
       body_page.style.width = "1200px";
       body_page.style.margin = "auto";
+      ele_footer.style.width = "1200px";
+      ele_footer.style.margin = "auto";
     } else {
       body_page.style.width = "100%";
+      ele_footer.style.width = "100%"
     }
   }
   window.addEventListener("DOMContentLoaded", updateWidth);
